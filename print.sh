d225f2c595ce8a592825ffd2699501b50b849c1e
girin <girinath.kalluru@gmail.com>
2024-02-27 15:25:09 +0530

    sample

--- print.sh
+++ print.sh
@@ -1,6 +1,6 @@
 echo Print syntax
 
-echo -e "\e[31m This is color \e[0m"
-echo -e "\e[32m This is color \e[0m"
-echo -e "\e[33m This is color \e[0m"
-echo -e "\e[34m This is color \e[0m"
+echo -e "\e[31m This is RED color \e[0m"
+echo -e "\e[32m This is GREEN color \e[0m"
+echo -e "\e[33m This is YELLOS color \e[0m"
+echo -e "\e[34m This is BLUE color \e[0m"
